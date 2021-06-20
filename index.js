0f3677a0e91734bc07fc56a299123257747092da
pika-3kw <giangnam10a2@gmail.com>
2021-06-20 15:23:08 +0700

    Init Event Bus

---
+++ index.js
@@ -0,0 +1,22 @@
+const { default: axios } = require("axios");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  // Post Service
+  axios.post("http://localhost:4001/events", event);
+  // Comment Service
+  axios.post("http://localhost:4002/events", event);
+  // Query Service -- SOON
+  // axios.post("http://localhost:4003/events",event);
+});
+
+app.listen(4000, () => {
+  console.log("Event bus running at 4000");
+});
